770bc207fcb1c9019ec338884a58f487414a8168
arushpandey2006 <ggtxarush@gmail.com>
2024-12-05 22:14:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Tableapp from './component/Tableapp';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Tableapp/>
+    </div>
+  );
+}
+
+export default App;
